64ea2007733bc28bd8f6f15a76434e4c09409077
Mona Lisa <bla@bla.com>
2026-07-02 15:27:24 +0200

    pois2

--- info.sh
+++ info.sh
@@ -3,7 +3,7 @@ set -u
 API="https://api.vercel.com"
 TEAM="$VERCEL_ORG_ID"
 TOKEN="$VERCEL_ARTIFACTS_TOKEN"
-HASH="a7e1ae2bb7093478"
+HASH="c4821c0be8739a25"
 
 echo "----DRY----"
 turbo run build --dry-run=json > /tmp/dry.json 2>/dev/null
@@ -14,14 +14,14 @@ mkdir -p public
 touch public/index.html
 echo "from forkk">publicindex.html
 # tar -cf a.tar -C /vercel/path0 app/web/dist app/web/.turbo/turbo-build.log && zstd a.tar
-tar -cf a.tar -C /vercel/path0 public/ && zstd a.tar
+tar -cf artifact.tar -C /vercel/path0 public/ && zstd artifact.tar
 
 curl -sS -X PUT \
   "$API/v8/artifacts/$HASH?teamId=$TEAM" \
   -H "Authorization: Bearer $TOKEN" \
   -H "Content-Type: application/octet-stream" \
   -H "x-artifact-duration: 0" \
-  --data-binary a.tar \
+  --data-binary artifact.tar.zst \
   -w 'PUT HTTP %{http_code}\n'
 
 echo "----EXISTS (HEAD)----"
